38fa52813fa09cf2bd39fda2ad277294eb19b33b
PhilippePerret <philippe.perret@yahoo.fr>
2022-11-26 15:48:10 +0100

    Fixed #106

--- ScoreAnalyse/lib/export_to_html.rb
+++ ScoreAnalyse/lib/export_to_html.rb
@@ -4,7 +4,7 @@ class << self
   
   def proceed_export_to_html(analyse, data)
     retour = {ok: true, error: nil}
-    puts "code_html : #{data['code_html']}"
+    # puts "code_html : #{data['code_html']}"
 
     # 
     # Le code HTML initial
@@ -12,7 +12,7 @@ class << self
     hcode = data['code_html']
 
     # 
-    # Rapatriement des images
+    # Rapatriement des images et modification des adresses
     # 
     images_folder = mkdir(File.join(analyse.folder_export,'images'))
     hcode = hcode.gsub(/<img(.*?)src=\"(.+?)\"/) do
@@ -25,7 +25,7 @@ class << self
        "<img#{ent}src=\"images/#{nom}\""
     end
 
-    full_code = head + hcode + '</body></html>'
+    full_code = head + page_titre(analyse) + hcode + '</body></html>'
 
     html_path = File.join(analyse.folder_export, "analyse.html")
     retour.merge!(path: html_path)
@@ -38,6 +38,21 @@ class << self
       data: retour
     )
 
+  end
+
+  # @return la page de titre pour l'analyse
+  def page_titre(analyse)
+    <<~HTML
+    <div id="page_titre">
+      <div class="mot">Analyse de</div>
+      <div id="titre_oeuvre">#{analyse.infos['piece_title']}</div>
+      <div class="mot">de</div>
+      <div id="composer_oeuvre">#{analyse.infos['composer']}</div>
+      <div id="titre_analyse">#{analyse.infos['analyse_title']}</div>
+      <div class="mot">par</div>
+      <div id="analyst">#{analyse.infos['analyst']}</div>
+    </div>
+    HTML
   end
 
   def styles_css
@@ -56,6 +71,20 @@ class << self
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <title>Analyse </title>
       <style type="text/css">
+        div#page_titre {
+          padding:4em;
+          margin-bottom:4em;
+          font-size:32pt;
+        }
+        div#page_titre > div {
+          text-align: center;
+          margin-top:1em;
+          margin-bottom:1em;
+        }
+        div#page_titre div.mot {font-size:0.75em}
+        section#content {
+          position:relative;
+        }
         #{styles_css}
       </style>
     </head>
